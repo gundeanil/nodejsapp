913aee966a379e3b674335f915b83ab38f6907d5
gundeanil <116259928+gundeanil@users.noreply.github.com>
2023-01-04 15:42:12 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,7 @@ sonarqubeScanner({
        options : {
 	    'sonar.projectDescription': 'Node JS application',
 	    'sonar.projectName': 'anilgunde_nodejsapp',
-	    'sonar.projectKey': 'anilgunde_nodejsapp'
+	    'sonar.projectKey': 'anilgunde_nodejsapp',
 	    'sonar.login': '1f2722a93b04c23afca79f6cdc6e59e6c3eaaa93',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
